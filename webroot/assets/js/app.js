5477fcad2c8fb6392f91417a83f545feed1673d9
zhunt <zhunt>
2018-11-12 19:26:04 -0500

    Set-up webpack.mix

---
+++ webroot/assets/js/app.js
@@ -0,0 +1,15 @@
+// app.js
+
+
+let hello = () => {
+    console.log('Hello');
+};
+
+let sum = (a,b) => { return a + b; };
+
+let prod = (a,b) => { return a*b; };
+
+console.log(`sum: ${sum(1,2)} `);
+console.log(`product: ${prod(1,2)} `);
+
+console.log(`product: ${prod(2,2)} `);
