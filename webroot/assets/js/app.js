8d3d963cf4c565e8e037a7ea71fd7c92fd79582c
zhunt <zhunt>
2018-11-12 23:54:27 -0500

    Add scss/Bootstrap to build

--- webroot/assets/js/app.js
+++ webroot/assets/js/app.js
@@ -1,6 +1,5 @@
 // app.js
 
-
 let hello = () => {
     console.log('Hello');
 };
@@ -13,3 +12,9 @@ console.log(`sum: ${sum(1,2)} `);
 console.log(`product: ${prod(1,2)} `);
 
 // console.log(`product: ${prod(2,2)} `);
+
+const var1 = [1,2,3];
+
+let var2 = [...var1].splice(',');
+
+console.log(var2);
